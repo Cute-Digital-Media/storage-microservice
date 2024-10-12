e6a08c0049111ce65fc6f3e8563c1f9f5466cf93
Omar <anaquac0@gmail.com>
2024-10-12 16:13:50 -0400

    feat: Agregar configuración de servicios de base de datos y caché

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,40 @@
-import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { FirebaseModule } from './firebase/firebase.module';
+import { Module } from '@nestjs/common';
+import { ImagesModule } from './images/images.module';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { CacheModule } from '@nestjs/cache-manager';
+import * as redisStore from 'cache-manager-redis-store';
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    ConfigModule.forRoot({ cache: true }),
+    FirebaseModule,
+    ImagesModule,
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      entities: [],
+      synchronize: true,
+    }),
+    CacheModule.register({
+      isGlobal: true,
+      store: redisStore,
+      host: 'localhost',
+      port: 6379,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
