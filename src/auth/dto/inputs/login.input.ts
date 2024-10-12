7f1d36e6eba674172f6efada24e4b969c6fbaedd
Omar <anaquac0@gmail.com>
2024-10-12 16:13:19 -0400

    feat: Agregar clases de entrada para autenticación
    Se agregaron las clases LoginInput y SignupInput en el directorio src/auth/dto/inputs. Estas clases contienen las propiedades necesarias para realizar el inicio de sesión y el registro de usuarios. Las clases están validadas utilizando class-validator para garantizar que los campos cumplan con los requisitos necesarios.
    
    Se agregó también el enum ValidRoles en el directorio src/auth/enums. Este enum define los roles válidos para los usuarios.
    
    Se implementaron los guards AuthGuard y RolesGuard en los directorios src/auth/guards. Estos guards se encargan de proteger las rutas y verificar la autenticación y los roles de los usuarios.
    
    Se creó la interfaz JwtPayload en el directorio src/auth/interfaces. Esta interfaz define la estructura del payload del token JWT.
    
    Por último, se agregó la clase AuthResponse en el directorio src/auth/types. Esta clase representa la respuesta de autenticación y contiene el token JWT y los datos del usuario.
    
    Estos cambios son necesarios para implementar la funcionalidad de autenticación en la aplicación.

---
+++ src/auth/dto/inputs/login.input.ts
@@ -0,0 +1,13 @@
+import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+
+export class LoginInput {
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @MinLength(6)
+  password: string;
+
+  @IsNotEmpty()
+  tenantId: string;
+}
