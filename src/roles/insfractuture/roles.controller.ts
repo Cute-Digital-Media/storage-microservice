c6b6cd7f6eb46a07f0399a59e4be9f71e1c5e661
RavenPoe1900 <ravenpoe1900@gmail.com>
2024-10-13 12:14:16 -0400

    feat: add swagger to controller

--- src/roles/insfractuture/roles.controller.ts
+++ src/roles/insfractuture/roles.controller.ts
@@ -8,22 +8,44 @@ import {
   Param,
   ParseIntPipe,
   Query,
+  HttpCode,
+  HttpStatus,
 } from '@nestjs/common';
 import { CreateRoleDto, UpdateRoleDto } from '../domain/role.dto';
 import { RolesService } from '../application/roles.service';
 import { RoleEntity } from '../domain/role.enity';
 import { PaginatedResponse } from 'src/_shared/domain/paginationResponse.dto';
 import { PaginationDto } from 'src/_shared/domain/pagination.dto';
+import { ApiTags } from '@nestjs/swagger';
+import { ApiResponseSwagger } from 'src/_shared/swagger/response.swagger';
+import {
+  createSwagger,
+  deleteSwagger,
+  findOneSwagger,
+  findSwagger,
+  updateSwagger,
+} from 'src/_shared/swagger/http.swagger';
 
+const controllerName = 'Roles';
+
+@ApiTags('Roles')
+@Controller({
+  path: 'roles',
+  version: '1',
+})
 @Controller('roles')
 export class RoleController {
   constructor(private readonly roleService: RolesService) {}
 
+  @HttpCode(HttpStatus.CREATED)
+  @ApiResponseSwagger(createSwagger(RoleEntity, controllerName))
   @Post()
   async create(@Body() createRoleDto: CreateRoleDto): Promise<RoleEntity> {
     return this.roleService.save(createRoleDto);
   }
 
+  @HttpCode(HttpStatus.OK)
+  @ApiResponseSwagger(findSwagger(RoleEntity, controllerName))
   @Get()
   async findAll(
     @Query() pagination: PaginationDto,
@@ -31,12 +53,16 @@ export class RoleController {
     return this.roleService.findAll(undefined, pagination);
   }
 
+  @HttpCode(HttpStatus.OK)
+  @ApiResponseSwagger(findOneSwagger(RoleEntity, controllerName))
   @Get(':id')
   async findOne(@Param('id', ParseIntPipe) id: number): Promise<RoleEntity> {
     return this.roleService.findOne({ where: { id } });
   }
 
   @Put(':id')
+  @HttpCode(HttpStatus.ACCEPTED)
+  @ApiResponseSwagger(updateSwagger(RoleEntity, controllerName))
   async update(
     @Param('id', ParseIntPipe) id: number,
     @Body() updateRoleDto: UpdateRoleDto,
@@ -45,6 +71,8 @@ export class RoleController {
     return this.roleService.findOne({ where: { id } });
   }
 
+  @HttpCode(HttpStatus.ACCEPTED)
+  @ApiResponseSwagger(deleteSwagger(null, controllerName))
   @Delete(':id')
   async remove(@Param('id', ParseIntPipe) id: number): Promise<void> {
     await this.roleService.delete({ id });
--- src/users/insfractuture/user.controller.ts
+++ src/users/insfractuture/user.controller.ts
@@ -9,23 +9,43 @@ import {
   Request,
   ParseIntPipe,
   Query,
+  HttpCode,
+  HttpStatus,
 } from '@nestjs/common';
 import { UserEntity } from '../domain/user.entity';
 import { CreateUserDto, UpdateUserDto } from '../domain/user.dto';
 import { UsersService } from '../application/users.service';
-import { RequestUser } from 'src/_shared/domain/request-user';
 import { PaginationDto } from 'src/_shared/domain/pagination.dto';
 import { PaginatedResponse } from 'src/_shared/domain/paginationResponse.dto';
+import { ApiTags } from '@nestjs/swagger';
+import { ApiResponseSwagger } from 'src/_shared/swagger/response.swagger';
+import {
+  createSwagger,
+  deleteSwagger,
+  findOneSwagger,
+  findSwagger,
+  updateSwagger,
+} from 'src/_shared/swagger/http.swagger';
 
-@Controller('users')
+const controllerName = 'Users';
+
+@ApiTags('Users')
+@Controller({
+  path: 'users',
+  version: '1',
+})
 export class UserController {
   constructor(private readonly userService: UsersService) {}
 
+  @HttpCode(HttpStatus.CREATED)
+  @ApiResponseSwagger(createSwagger(UserEntity, controllerName))
   @Post()
   async create(@Body() createUserDto: CreateUserDto): Promise<UserEntity> {
     return this.userService.createUser(createUserDto);
   }
 
+  @HttpCode(HttpStatus.OK)
+  @ApiResponseSwagger(findSwagger(UserEntity, controllerName))
   @Get()
   async findAll(
     @Query() pagination: PaginationDto,
@@ -33,11 +53,15 @@ export class UserController {
     return this.userService.findAll(undefined, pagination);
   }
 
+  @HttpCode(HttpStatus.OK)
+  @ApiResponseSwagger(findOneSwagger(UserEntity, controllerName))
   @Get(':id')
   async findOne(@Param('id', ParseIntPipe) id: number): Promise<UserEntity> {
     return this.userService.findOne({ where: { id } });
   }
 
+  @HttpCode(HttpStatus.ACCEPTED)
+  @ApiResponseSwagger(updateSwagger(UserEntity, controllerName))
   @Put(':id')
   async update(
     @Param('id', ParseIntPipe) id: number,
@@ -47,6 +71,8 @@ export class UserController {
     return this.userService.findOne({ where: { id } });
   }
 
+  @HttpCode(HttpStatus.ACCEPTED)
+  @ApiResponseSwagger(deleteSwagger(null, controllerName))
   @Delete(':id')
   async remove(@Param('id', ParseIntPipe) id: number): Promise<void> {
     await this.userService.delete({ id });
