2d1cd9eb02f7a0ab60c2ecc0540732bd3acba5c6
saulhermida <saulhg2015@gmail.com>
2024-10-14 13:37:28 -0400

    fix(user):fixjwt guard to return user

--- src/modules/config/auth/app/guards/jwtGuards.ts
+++ src/modules/config/auth/app/guards/jwtGuards.ts
@@ -1,6 +1,16 @@
 
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  handleRequest(err, user, info, context: ExecutionContext) {
+    if (err || !user) {
+      throw err || new UnauthorizedException();
+    }
+    // Aquí aseguras que req.user esté disponible
+    const request = context.switchToHttp().getRequest();
+    request.user = user;
+    return user;
+  }
+}
